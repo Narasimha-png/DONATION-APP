3450df83dcb8c8df3293e1f61de9c430151eb34c
Narasimha-png <narasimhas9490@gmail.com>
2024-10-12 14:10:32 +0530

    Donation-App v1

---
+++ models/Donation.js
@@ -0,0 +1,9 @@
+// server/models/Donation.js
+const mongoose = require('mongoose');
+
+const donationSchema = new mongoose.Schema({
+    amount: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Donation', donationSchema);
